9a47ab76f56d7a863f6cf5bee94d6d0323610fb9
Pablo Luesia <698387@github.com>
2020-01-16 13:41:56 +0100

    Detector simple

--- python-detector/CellDetector.py
+++ python-detector/CellDetector.py
@@ -1,10 +1,17 @@
 import cv2 as cv
-import math
+import sys
 from CellPointDetector import pointInCell, allContourPoints
-
+from skeletonize import find_skeleton
+import statistics as st
+import numpy as np
 
 # Reads the videofile
-data_file = input('Name of the file: ')
+# Select the videofile
+if (len(sys.argv) < 2):
+    data_file = input('Name of the file: ')
+else:
+    data_file = sys.argv[1]
+
 
 v = cv.VideoCapture(data_file)
 
@@ -16,22 +23,55 @@ if not v.isOpened():
 # Extracts each frame
 stop = -1
 while stop == -1:
-    raw_image_state, raw_image = v.read()
+    raw_image_state, raw_img = v.read()
     if not raw_image_state:
         v.release()
         break
 
-    gray_image = cv.cvtColor(raw_image, cv.COLOR_BGR2GRAY)
-    blur_image = cv.GaussianBlur(gray_image, (5,5), 0)  # Eliminates noise
+    gray_img = cv.cvtColor(raw_img, cv.COLOR_BGR2GRAY)
+    equalized_img = cv.equalizeHist(gray_img)              # Equalized the hist
+    blur_img = cv.GaussianBlur(equalized_img, (9,9), 0)         # Eliminates noise
+    binary_img = cv.adaptiveThreshold(blur_img, 255,
+                                         cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                         cv.THRESH_BINARY,11,-3)
+    contours, hierarchy = cv.findContours(binary_img, cv.RETR_LIST,
+                                          cv.CHAIN_APPROX_NONE )
     
-    # Extracts edges with canny detector
+    contour_lengths = list(map(lambda x: len(x), contours))
+    mean = st.mean(contour_lengths)     # Mean of the data
+    sd = st.pstdev(contour_lengths)
+    
+    contour_length_thres = mean + sd
+            
+    good_contours = list(filter(lambda x: len(x) > contour_length_thres, contours))
+    
+    found_contours = cv.drawContours(raw_img.copy(), good_contours, -1,
+                                     (0,255,0))
+    
+    """params = cv.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = 1500
+    detector = cv.SimpleBlobDetector(params)
+    blobs = detector.detect(binary_img)
+    
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+    # the size of the circle corresponds to the size of blob
+    
+    im_with_keypoints = cv.drawKeypoints(raw_img, blobs, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    
+    # Show blobs
+    cv.imshow("Keypoints", im_with_keypoints)
+    cv.waitKey(0)"""
+    
+    """# Extracts edges with canny detector
     thrs1 = 3000
     thrs2 = 2*thrs1
-    edges = cv.Canny(blur_image, thrs1, thrs2, apertureSize=7)  
+    edges = cv.Canny(equalized_img, thrs1, thrs2, apertureSize=7)  
 
     contours, hierarchy = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_NONE )  
 
-    found_contours = cv.drawContours(raw_image.copy(), contours, -1, (0,255,0)) 
+    found_contours = cv.drawContours(raw_img.copy(), contours, -1, (0,255,0)) 
     
     points = allContourPoints(contours,5)
     for p in points:
@@ -43,12 +83,16 @@ while stop == -1:
     found_points = raw_image.copy()
     for p in points:
         found_points = cv.circle(found_points, p[1],0, (0,255,255))
-    
-    cv.imshow('original', gray_image)
-    cv.imshow('edge', edges)
-    cv.imshow('Contours', found_contours)
-    cv.imshow('Points', found_points)
 
-    stop = cv.waitKey(5)
+    # cv.imshow('Points', found_points) 
+    cv.imshow('edge', edges)"""
+    
+    cv.imshow('Contours', found_contours)
+    cv.imshow('original', gray_img)
+    cv.imshow('Equalized', equalized_img)
+    cv.imshow('blurred', blur_img)
+    cv.imshow('binary', binary_img)
+
+    stop = cv.waitKey(0)
 
 cv.destroyAllWindows()
