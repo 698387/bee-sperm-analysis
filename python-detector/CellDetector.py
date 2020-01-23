c92fb036475fc4d83d6dac17e1f713a938397bac
Pablo Luesia <pluesia@github.com>
2020-01-23 14:23:03 +0100

    Line decoupler with tendence

--- python-detector/CellDetector.py
+++ python-detector/CellDetector.py
@@ -7,6 +7,7 @@ import numpy as np
 from line_decoupler import decouple_lines, follow_line
 import math
 from sklearn.neighbors import KDTree
+import random as r
 
 # list of colors
 v = [0, 63, 127, 191, 255]
@@ -45,39 +46,26 @@ while stop == -1:
                                      cv.ADAPTIVE_THRESH_GAUSSIAN_C,
                                      cv.THRESH_BINARY,11,-1)
     skeleton, _ = find_skeleton(binary_img)
+    
 
     lines = decouple_lines(skeleton,
-                           max_distance = 35,
-                           min_length = 1, 
-                           max_angle = math.pi/8)
-    
+                           max_distance = 20,
+                           min_length = 1,
+                           max_angle = math.pi/4)
     lines_img = raw_img.copy()
+    
     for i in range(0, len(lines)):
         cv.polylines(lines_img, 
                      np.int32([lines[i]]), 
                      False, 
                      colors[i % num_colors])
     
-    """contours, _ = cv.findContours(skeleton, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-    contour_cod = []
-    for i in range(0, len(contours)):
-        circles_img = raw_img.copy()
-        contour_img = cv.drawContours(np.zeros(gray_img.shape, dtype=np.uint8), contours, i, 255)
-        cv.drawContours(circles_img, contours, i, (255,0,0))
-        # contour_cod.append(cv.HoughCircles(contour_img, cv.HOUGH_GRADIENT, 1, 5))
-        circles = cv.HoughCircles(contour_img, cv.HOUGH_GRADIENT, 1, 5)
-        if circles != None:
-            for c in circles:
-                center = (c[0], c[1])
-                r = c[2]
-                cv.circle(circles_img, center, r, (0,255,0))  
-        cv.imshow('circles detected', circles_img)
-        cv.waitKey(0)""" 
     skeleton_img = raw_img.copy()
     skeleton_img[np.where(skeleton==[255])] = [0,0,255]
     cv.imshow('skeletons', skeleton_img)
     cv.imshow('lines', lines_img)
 
-    stop = cv.waitKey(0)
+    stop = cv.waitKey(1)
+    # cv.waitKey(0)
 
 cv.destroyAllWindows()
--- python-detector/line_decoupler.py
+++ python-detector/line_decoupler.py
@@ -2,123 +2,185 @@ import cv2 as cv
 import numpy as np
 import math as m
 from sklearn.neighbors import KDTree
+import random as r
 
-PI_CUARTER = m.pi/4
-
-# Sets unaviable the zone near to the point
-def zone_unaviable(point, point_tree, available_points, radius = 2):
+"""
+Sets unaviable the zone near to the point
+"""
+def set_zone_unavailable(point, point_tree, available_points, radius = 3):
     idx2unaviable = point_tree.query_radius([point], 
                                             radius,
                                             return_distance = False)[0]
     for idx in idx2unaviable:
         available_points[idx] = False
-
-""" 
-Follow the line which cross the point origin, in both directions
+###############################################################################
+    
 """
-def follow_line(origin, point_tree, all_points, available_points, max_distance,
-                max_angle):
-    line = np.array([origin])
-    point = origin
+Extracts the parameters to extract the next point, depending on the previous
+line
+"""
+def get_pred_point_params(point, line, num_point_theta):
+    # Direction of the line at this point
+    v_dir = point - line[-1]
+    u_dir = v_dir / np.linalg.norm(v_dir)
+    # Vector of the force that curves the line
+    v_f = point - line[-2]
+    dist = np.linalg.norm(v_f)
+    f = line[-2] / dist - u_dir / dist
+    return u_dir, f
+    return sum(map(lambda x:
+                   m.atan2(point[1] - x[1], point[0] - x[0]),
+                   line[-num_point_theta:-1])) / num_point_theta
+###############################################################################
+
+"""
+Returns a metric indicating how good is the point up to the direction 
+parameters, for the point origin
+"""
+def dir_metric(point, origin, dir_params):
+    # Predict a position for the point
+    dist = np.linalg.norm(point - origin)
+    pred_p = origin + dist *(dir_params[0] + dist*dir_params[1])
+    # Distance to the prediction point as metric
+    return np.linalg.norm(pred_p - point)
+###############################################################################
+        
+"""
+Initialize the line with ransac from a set of points
+"""
+def init_line(point, all_points, point_tree, available_points,
+              max_distance, num_point_theta):
+    # Select the available near candidates that 
+    idx_candidates = point_tree.query_radius([point], max_distance)[0]
+    candidates = [all_points[i] for i in idx_candidates 
+                  if available_points[i] and (all_points[i] != point).all()]
+
+    # Perform RANSAC to extract the direction
+    hits = 0
+    dir_params = get_pred_point_params(point, point, 0)
+    max_tries = len(candidates)
+    tries = 0
+    line = [point]       # Line
+    while(hits < num_point_theta and tries < max_tries):
+        tries = tries + 1
+        line = [point]
+        # Select 2 random points
+        r_p = r.choices(candidates, k=2)
+        r_p.sort(key = lambda x: np.linalg.norm(x - point))
+        dir_params = get_pred_point_params(point, r_p, 0)
+        hits = 0
+        # Check the direction with the other points
+        for p in candidates:
+            metric = dir_metric(p, point, dir_params)
+            if metric < 3:
+                hits = hits + 1
+                line.append(p)
+                
+    if hits < num_point_theta:  # No direction has been found
+        line = [point]
+        
+    # Set the zone around the line unavailable
+    for p in line:
+        set_zone_unavailable(p, point_tree, available_points)
+        
+    # Sort the line depending on the distance to the point
+    line.sort(key = lambda x: np.linalg.norm(x - point))
+    return line, dir_params
+###############################################################################
+
+"""
+Give a metric depending on the direction and the distance
+"""
+def inv_rank(candidate, max_dist):
+    return ( (candidate[2]**2)/max_dist**2 ) \
+            * ( (candidate[3]**2)/(max_dist*2)**2 )
+###############################################################################
     
+"""
+Generate all candidate points
+Candidates are nearer than max_distance, and the direction to them from the
+last known point doesn't form more than max_angle degrees. The returned 
+array is sorted by the distance to the objective
+"""
+def generate_candidates(point, dir_params, all_points, point_tree, 
+                        available_points, max_distance, max_angle):
     # Indices of all the near points
-    idx_near_points = point_tree.query_radius([point], max_distance,
-                                              return_distance = True)
-    # The nearest points which are available
-    idx_near_available = [(idx_near_points[0][0][i], idx_near_points[1][0][i])  
-                          for i in range(0, len(idx_near_points[0][0])) 
-                          if available_points[idx_near_points[0][0][i]]]
-    if not idx_near_available:
-        return line             # No near points
+    idx_candidates = point_tree.query_radius([point], max_distance,
+                                             return_distance = True)
+    # The near points which are available
+    all_candidates = \
+    [(idx_candidates[0][0][i],                  # index
+      all_points[idx_candidates[0][0][i]],      # point
+      idx_candidates[1][0][i])                  # distance                 
+     for i in range(0, len(idx_candidates[1][0])) \
+     if available_points[idx_candidates[0][0][i]]]
     
-    # Inicializate with the nearest point
-    nearest_idx, _ = min(idx_near_available, key = lambda x: x[1])
+    # Generate differences with the theta from to the points direction
+    directions_diff = list(map(lambda x: dir_metric(x[1], point, dir_params),
+                               all_candidates) )
+    # Select points in the range (-max_angle, max_angle) of the direction
+    return np.asarray(
+            [(all_candidates[idx][0],   # index
+              all_candidates[idx][1],   # point
+              all_candidates[idx][2],   # distance
+              alpha )                   # direction diff 
+            for idx, alpha in list(enumerate(directions_diff))
+            if alpha < max_angle ])
+###############################################################################
     
-    available_points[nearest_idx] = False
-    point = all_points[nearest_idx]
+"""
+Update the point, the direction and the line of line with the most valuable
+of the candidates candidates. The direction will be calculated with the 
+mean of num_point_theta points
+"""    
+def update_line(line, candidates, point_tree, available_points,
+                num_point_theta, max_distance):
+    # Gives a value depending on the direction and the distance
+    i = np.argmin(
+            list(map(lambda x: inv_rank(x, max_distance), candidates)) )
+    point = candidates[i][1]
+    # The zone near to the point is now unavailable
+    set_zone_unavailable(point, point_tree, available_points)
+    # Calculates a mean for the new direction
+    dir_params = get_pred_point_params(point, line, num_point_theta)
     line = np.concatenate((line, [point]))
-    theta = m.atan2(point[1] - origin[1], point[0] - origin[0])
+    return (point, dir_params, line)
+###############################################################################
     
-    """
-    Generate all candidate points
-    Candidates are nearer than max_distance, and the direction to them from the
-    last known point doesn't form more than max_angle degrees. The returned 
-    array is sorted by the distance to the objective
-    """
-    def generate_candidates():
-        #######################################################################
-        # Indices of all the near points
-        idx_candidates = point_tree.query_radius([point], max_distance,
-                                                 return_distance = True)
-        # The near points which are available
-        all_candidates = \
-        [(idx_candidates[0][0][i],                  # index
-          all_points[idx_candidates[0][0][i]],      # point
-          idx_candidates[1][0][i])                  # distance                 
-         for i in range(0, len(idx_candidates[1][0])) \
-         if available_points[idx_candidates[0][0][i]]]
-        # Generate all the directions to the points
-        directions = list(map(lambda x: m.atan2(x[1][1]-point[1],
-                                                x[1][0]-point[0]),
-                              all_candidates) )
-        # Select points in the range (-max_angle, max_angle) of the direction
-        return np.asarray(
-                [(all_candidates[idx][0],   # index
-                  all_candidates[idx][1],   # point
-                  all_candidates[idx][2],   # distance
-                  abs(alpha - theta))       # direction diff
-                for idx, alpha in list(enumerate(directions)) \
-                if abs(alpha - theta) < max_angle])
-        #######################################################################
+""" 
+Follow the line which cross the point in the origin index, in both directions
+"""
+def follow_line(origin, point_tree, all_points, available_points, 
+                max_distance, max_angle):
+    # Initialize the line
+    line, dir_params = init_line(origin, all_points, point_tree, 
+                                 available_points, max_distance, 5)
+    # Has it been correctly initializied
+    if len(line) == 1:
+        return line
     
-    """
-    Update the point, the direction and the line of line with the most valuable
-    of the candidates candidates. The direction will be calculated with the 
-    mean of num_point_theta points
-    """    
-    def update_line(line, candidates, num_point_theta):
-        #######################################################################
-        # Gives a value depending on the direction and the distance
-        i = np.argmax(map(lambda x: \
-                          1/(x[2]*x[3]*x[3]),
-                          candidates))
-        global_idx = candidates[i][0]
-        available_points[global_idx] = False        # Point no longer available
-        point = candidates[i][1]
-        zone_unaviable(point, point_tree, available_points)
-        # Calculates a mean for the new theta
-        theta = sum(map(lambda x: \
-                        m.atan2(point[1] - x[1], point[0] - x[0]),
-                        line[-num_point_theta:-1])) / (num_point_theta - 1)
-        line = np.concatenate((line, [point]))
-        return (point, theta, line)
-        #######################################################################
-        
+    # The new point is the last of the line
+    point = line[-1]
     found_end = False
-    for i in range(0,3):
-        candidates = generate_candidates()
-        if len(candidates) == 0:
-            found_end = True
-            break
-        point, theta, line = update_line(line, candidates, i + 2)
-        
     # Keep going until it reaches the end of the line
     while(True):
-        candidates = generate_candidates()
+        candidates = generate_candidates(point, dir_params, all_points, 
+                                         point_tree, available_points, 
+                                         max_distance, max_angle)
         if len(candidates) > 0:                 # There are candidates
-            point, theta, line = update_line(line, candidates, 5)
-        elif not found_end:                     # Reaches the end of the line
+            point, dir_params, line = update_line(line, candidates, point_tree, 
+                                                  available_points, 
+                                                  min(len(line), 4), max_distance)
+        elif not found_end:                     # Reaches the first extrem
             # Change the direction of the search
             point = origin
-            theta = sum(map(lambda x: \
-                        m.atan2(point[1] - x[1], point[0] - x[0]),
-                        line[1:5])) / 4
             line = np.flip(line, axis=0)
+            dir_params = get_pred_point_params(point, line, min(len(line), 4))
             found_end = True
             continue
-        else:                                   # End of the other line
+        else:                                   # End of the other extrem
             break
-
+        
     return line
 
 """ 
@@ -130,8 +192,7 @@ white
 @max_angle is the maximum angle to consider that it is to hard the curve to be
             part of the same line (in radians)
 """
-def decouple_lines(img, max_distance = 10, min_length = 10, max_angle=m.pi/4):
-
+def decouple_lines(img, max_distance = 10, min_length = 10, max_angle=m.pi/6):
     t_all_pixels = np.where(img == [255])
     # All the white pixels
     all_pixels = np.asarray([t_all_pixels[1],
@@ -140,23 +201,21 @@ def decouple_lines(img, max_distance = 10, min_length = 10, max_angle=m.pi/4):
     # Generates the kd-tree
     pixel_tree = KDTree(all_pixels, metric = 'euclidean')
     
-    lines = []                               # Detected lines
+    r.seed()                                # Initialize the random seed
+    lines = []                              # Detected lines
     while any(available_pixels):
         # Beginning point
         origin_idx = np.where(available_pixels)[0][0]
-        available_pixels[origin_idx] = False
-        origin = all_pixels[origin_idx]
-        
         
         # Find the line that contains the beginning point
-        line = follow_line(origin, 
+        line = follow_line(all_pixels[origin_idx], 
                            pixel_tree,
                            all_pixels,
                            available_pixels,
-                           max_distance, 
+                           max_distance,
                            max_angle)
         
-        if len(line) >= min_length:        # It has found a line
+        if len(line) >= min_length:         # It has found a line
             lines.append(line)
 
     return lines
